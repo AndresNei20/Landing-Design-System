8a714944c7981dad2dce232fcc75c2005abe5e3a
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-06-02 01:25:38 -0500

    p

--- landing-ds/src/components/ValueCards/ValueCards.jsx
+++ landing-ds/src/components/ValueCards/ValueCards.jsx
@@ -25,10 +25,10 @@ export const ValueCards = () => {
 
   return (
     <div className='v-cards-wrap' ref={cardsRef}>
-      <ValueCard title="Target Audience" img="../../../../public/targeticon.svg" hoverIcon="../../../../public/targeticon2.svg" text="People who are unbanked or underbanked, a design system that helps you to improve your financial idea"></ValueCard>
+      <ValueCard title="Target Audience" img="https://i.postimg.cc/kB4tJz0q/target.png" hoverIcon="https://i.postimg.cc/MXtQ8qr9/targethover.png" text="People who are unbanked or underbanked, a design system that helps you to improve your financial idea"></ValueCard>
       <ValueCard title="Design Tokens" img="../../../../public/tokensicon.svg" hoverIcon="../../../../public/tokensicon2.svg" text="Tokenized components with figma variables like colors, spacing and typography"></ValueCard>
       <ValueCard title="Dark Mode" img="../../../../public/moonicon.svg" hoverIcon="../../../../public/moonicon2.svg" text="Enable dark mode with a single click, enable to use our components on different modes"></ValueCard>
-      <ValueCard title="Personality" img="../../../../public/personalityicon.svg" hoverIcon="../../../../public/personalityicon2.svg" text="Friendly finance assistant, ideal if your looking more than the usual components"></ValueCard>
+      <ValueCard title="Personality" img="https://i.postimg.cc/G4T8YY3f/Outline-1.png" hoverIcon="https://i.postimg.cc/VdV0d4wM/Outline.png" text="Friendly finance assistant, ideal if your looking more than the usual components"></ValueCard>
       <ValueCard title="New Styles" img="../../../../public/staricon.svg" hoverIcon="../../../../public/staricon2.svg" text="New look to the famous Daviplata app, a different idea which make a new identity "></ValueCard>
       <ValueCard title="Simplified Tasks" img="../../../../public/removeicon.svg" hoverIcon="../../../../public/removeicon2.svg" text="Ideal for all type of desings, not just for one area. You can match it with all!"></ValueCard>
     </div>
